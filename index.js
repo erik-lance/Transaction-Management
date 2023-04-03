09ac073f758330e63736e562d07a42260cc89445
erik-lance <eriklance@gmail.com>
2023-04-03 20:12:31 +0800

    Additional initial

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express'); // Import express
 const app = express();              // Create express app
 const port = 3000;                  // Define port
 
+// Serve static files
+app.use(express.static('public'));
+
+
 // Start server
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
 
 // Start server
