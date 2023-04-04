da0f923b3c022ebdcd0dd4bdf940ecd1f51f2169
erik-lance <eriklance@gmail.com>
2023-04-04 20:35:50 +0800

    Added temporary conn.js

---
+++ models/conn.js
@@ -0,0 +1,34 @@
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./.env" });
+
+const node_1 = mysql.createConnection({
+    host: process.env.NODE_1_HOST,
+    port: process.env.NODE_1_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+});
+
+/*
+const node_2 = mysql.createConnection({
+    host: process.env.NODE_2_HOST,
+    port: process.env.NODE_2_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+});
+
+const node_3 = mysql.createConnection({
+    host: process.env.NODE_3_HOST,
+    port: process.env.NODE_3_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+});
+
+*/
+
+
+module.exports = { node_1 };
