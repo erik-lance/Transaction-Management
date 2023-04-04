f534b2fa33acf9aa7bf2f4cf0ba26ac34edfd70d
erik-lance <eriklance@gmail.com>
2023-04-04 20:35:36 +0800

    Added hbs view engine and routing

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-const express = require('express'); // Import express
-const app = express();              // Create express app
-const port = 3000;                  // Define port
+const express = require("express"); // Import express
+const exphbs = require("express-handlebars"); // Import express-handlebars
+const mysql = require("mysql"); // Import mysql
+const dotenv = require("dotenv"); // Import dotenv
+
+const hbs = exphbs.create({
+    defaultLayout: "main",
+    extname: "hbs",
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials",
+})
+
+const app = express(); // Create express app
+const port = 3000; // Define port
 
 // Serve static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+// Set view engine
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+
+// Set views
+//app.set("views", __dirname + "/views");
+
+// Import routes
+const routes = require("./routes/routes.js");
+
+// Routes
+app.use("/", routes);
 
 // Start server
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
 
-
-// Start server
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+module.exports = app;
