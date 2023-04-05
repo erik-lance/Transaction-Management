f859103b42fe0aa760b873e707427d8d5556de59
erik-lance <eriklance@gmail.com>
2023-04-05 18:41:25 +0800

    Filled in placeholders for other connections

--- models/conn.js
+++ models/conn.js
@@ -5,18 +5,11 @@ dotenv.config({ path: "./.env" });
 
 let node_host, node_user, node_password, node_database = [];
 
-if (process.env.NODE_NUM_CONFIGURATION == -1) {
-    node_host = process.env.NODE_SELF_HOST;
-    node_user = process.env.NODE_SELF_USER;
-    node_password = process.env.MYSQL_PASSWORD;
-    node_database = process.env.MYSQL_DATABASE;
-}
-
 switch (process.env.NODE_NUM_CONFIGURATION) {
     case -1:
         node_host = process.env.NODE_SELF_HOST;
         node_user = process.env.NODE_SELF_USER;
-        node_password = process.env.MYSQL_PASSWORD;
+        node_password = process.env.NODE_SELF_PASS;
         node_database = process.env.MYSQL_DATABASE;
         break;
     case 1:
@@ -24,6 +17,25 @@ switch (process.env.NODE_NUM_CONFIGURATION) {
         node_user = process.env.MYSQL_USER;
         node_password = process.env.MYSQL_PASSWORD;
         node_database = process.env.MYSQL_DATABASE;
+        break;
+    case 2:
+        node_host = process.env.NODE_2_HOST;
+        node_user = process.env.MYSQL_USER;
+        node_password = process.env.MYSQL_PASSWORD;
+        node_database = process.env.MYSQL_DATABASE;
+        break;
+    case 3:
+        node_host = process.env.NODE_3_HOST;
+        node_user = process.env.MYSQL_USER;
+        node_password = process.env.MYSQL_PASSWORD;
+        node_database = process.env.MYSQL_DATABASE;
+        break;
+    default:
+        node_host = process.env.NODE_SELF_HOST;
+        node_user = process.env.NODE_SELF_USER;
+        node_password = process.env.NODE_SELF_PASS;
+        node_database = process.env.MYSQL_DATABASE;
+        break;
 }
 
 const node_self = mysql.createConnection({
@@ -33,36 +45,6 @@ const node_self = mysql.createConnection({
     database: node_database,
 
 });
-
-/*
-const node_1 = mysql.createConnection({
-    host: process.env.NODE_1_HOST,
-    port: process.env.NODE_1_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-});
-*/
-
-/*
-const node_2 = mysql.createConnection({
-    host: process.env.NODE_2_HOST,
-    port: process.env.NODE_2_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-});
-
-const node_3 = mysql.createConnection({
-    host: process.env.NODE_3_HOST,
-    port: process.env.NODE_3_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-});
-
-*/
-
 
 // To avoid fragments or crashes, we need to make sure
 // we close the connection when the process is terminated
