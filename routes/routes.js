7c666c600ddd85d0923cc755734a6444bffee73b
erik-lance <eriklance@gmail.com>
2023-04-07 10:42:28 +0800

    Removed path

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,15 @@
 const express = require('express'); // Import express
-const path = require('path');
 
 const controller = require('../controllers/controller.js');
 
 const app = express();
 
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', './views');
 
 // GET
-app.get('/', controller.home);
-app.get('/movies', controller.movies);
-app.get('/moviesData', controller.moviesData);
+app.get('/', controller.home);                  // Gets home page
+app.get('/movies', controller.movies);          // Gets movies page
+app.get('/moviesData', controller.moviesData);  // Gets data on movies for DataTables
 
 // POST
 // app.post('/addMovie', controller.addMovie);
