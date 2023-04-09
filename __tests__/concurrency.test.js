3fff0b3cd9a89458a3e4ed9d88b58527f91e5b4a
erik-lance <eriklance@gmail.com>
2023-04-09 20:25:22 +0800

    Added note reminder

--- __tests__/concurrency.test.js
+++ __tests__/concurrency.test.js
@@ -1,6 +1,8 @@
 const mysql = require("mysql2/promise");
 const dotenv = require("dotenv").config({path: './.env'});
 
+// For each case, set the isolation level to read uncommitted, read committed, read repeatable and serializable.
+
 describe('Concurrent Transactions in two or more nodes reading the same item', () => {
     test.todo('Node 1 and node 2 are reading the same item');
     test.todo('Node 2 and node 3 are reading the same item');
