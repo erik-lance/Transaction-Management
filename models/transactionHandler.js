820145c418fafe71b0cf98f5b0746db372a5dd41
erik-lance <eriklance@gmail.com>
2023-04-16 02:43:03 +0800

    Added a recoverTransactions function which recovers all logs

--- models/transactionHandler.js
+++ models/transactionHandler.js
@@ -3,6 +3,88 @@ const retry = require("retry");
 const dotenv = require("dotenv").config({path: './.env'});
 const conn = require("../models/conn.js");
 
+/**
+ * This function is used to recover transactions that were not
+ * successfully committed to the database. It starts inserting
+ * transactions from the local logs into the database.
+ * 
+ * If the transaction is successfully committed, else, it is
+ * left in the local logs.
+ */
+function recoverTransactions() {
+    const [node_1_logs] = []
+    const [node_2_logs] = []
+    const [node_3_logs] = []
+
+    // Get node 1 logs
+    conn.dbQuery(conn.node_1, "SELECT * FROM mco2_logs", [], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            for (let i = 0; i < result.length; i++) {
+                node_1_logs.push(result[i]);
+            }
+        }
+    });
+
+    // Get node 2 logs
+    conn.dbQuery(conn.node_2, "SELECT * FROM mco2_logs", [], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            for (let i = 0; i < result.length; i++) {
+                node_2_logs.push(result[i]);
+            }
+        }
+    });
+
+    // Get node 3 logs
+    conn.dbQuery(conn.node_3, "SELECT * FROM mco2_logs", [], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            for (let i = 0; i < result.length; i++) {
+                node_3_logs.push(result[i]);
+            }
+        }
+    });
+
+    // Inserts the logs into the database
+    for (let i = 0; i < 3; i++) {
+        let node = [];
+        let node_logs = [];
+        switch(i) {
+            case 0:
+                node = conn.node_1;
+                node_logs = node_1_logs;
+                break;
+            case 1:
+                node = conn.node_2;
+                node_logs = node_2_logs;
+                break;
+            case 2:
+                node = conn.node_3;
+                node_logs = node_3_logs;
+            default:
+                console.log("Error: Unknown node");
+                break;
+        }
+
+        for (let j = 0; j < node_logs.length; j++) {
+            let log = node_logs[i];
+            let query = "INSERT INTO movies (name, year, `rank`, genre) VALUES (?, ?, ?, ?)";
+            let content = [log.name, log.year, log.rank, log.genre];
+            conn.dbQuery(conn.node, query, content, (err, result) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Recovered transaction: "+result);
+                }
+            });
+        }
+    }
+}
+
 function storeQuery(dbPool, query, content) {
     let t_type = query.split(" ")[0];   // Get the first word of the query
     let t_dest = [];    // Destination node(s) for the transaction
