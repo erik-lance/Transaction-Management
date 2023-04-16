e85fa68ccc8a30364a3de627f633b2d6fbc6ad46
Clyla <clyla_a_rafanan@dlsu.edu.ph>
2023-04-16 20:01:26 +0800

    show node availability

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,6 +7,20 @@ $(document).ready(function () {
         success: function (data) {
             // Print for now
             console.log(data);
+
+            $.each(data, function(index, value) {
+              const className = ".avail" + (index + 1);
+              if (value == 1) {
+                status = "ONLINE";
+                $(className).css('color', 'green');
+              }
+              else {
+                status = "OFFLINE";
+                $(className).css('color', 'grey');
+              }
+              $(className).text(status);
+            }
+
         },
         error: function (err) {
             console.log(err);
